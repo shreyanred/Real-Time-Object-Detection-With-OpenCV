4892b804fc4c644806b63a4a5f901300c30edada
shreyanred <shreyanreddy221@gmail.com>
2024-09-25 14:40:19 +0530

    Update real_time_object_detection.py

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -39,7 +39,7 @@ args = vars(ap.parse_args())
 CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-           "sofa", "train", "tvmonitor"]
+           "sofa", "train", "tvmonitor","phone","laptop","card","case",]
 
 # Assigning random colors to each of the classes
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
